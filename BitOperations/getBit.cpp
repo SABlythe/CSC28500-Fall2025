e676c78965fc2c935ffbf7ccb146445e62d92a27
Stephen Blythe <sblythe@lindenwood.edu>
2025-09-15 12:34:29 -0500

    finished getBit code

--- BitOperations/getBit.cpp
+++ BitOperations/getBit.cpp
@@ -8,7 +8,34 @@ int main(int argc, char *argv[])
   unsigned int number, position;
   cin >> number >> position;
 
+  cout << "original:";
+  printBinary(number);
+  cout << endl;
+
+  // build "mask" with single 1 in it
+  unsigned int mask=1;
+  mask = mask << position; 
+
+  cout << "    mask:";
+  printBinary(mask);
+  cout << endl;
   
+  
+  // bit-wiser "and" mask with the number
+  unsigned int answer = number & mask;
+
+  cout << "  and-ed:";
+  printBinary(answer);
+  cout << endl;
+  
+  // move answer "bit" into rightmost position
+  answer = answer >> position;
+
+  cout << "right sh:";
+  printBinary(answer);
+  cout << endl;
+
+  cout << "Extracted bit is :" << answer << endl;
   
   return 0;
 }
